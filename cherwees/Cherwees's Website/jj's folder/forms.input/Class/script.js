b44b0e50ce6dfc7739dfc9232a369fbe568b9394
Kraehe <gigi.giggles01@gmail.com>
2026-05-09 14:37:40 -0400

    added cherwees, renamed angelina

---
+++ cherwees/Cherwees's Website/jj's folder/forms.input/Class/script.js
@@ -0,0 +1,17 @@
+function submiteForm(form){
+    let name= form.name.value;
+    let =form.email.value; 
+
+    alert("name: " )
+}
+
+
+
+
+fuction makecommmet(name comment){
+    const comment_contain it =doccument.createElement("div")
+    constant comment_inner= <span class="_name>$[\span name"
+
+comment_container .classList .add("comment")
+
+}
---
+++ cherwees/Cherwees's Website/script.js
@@ -0,0 +1,22 @@
+// variable
+let faceEmoji = document.getElementById("face"); n      
+
+
+
+// syntax for functions:
+// function functionName() ( actual functions }
+function restless() {
+     faceEmoji.innerText = "😒";
+}
+function annoyed() {
+     faceEmoji.innerText = "🙄";
+}
+function cool() {
+    faceEmoji.innerText = "😎";
+}
+function tired() {
+    faceEmoji.innerText = "😵";
+}
+function whiny() {
+    faceEmoji.innerText = "😫";
+}
